371e04288695d0398bc2cd84a9c77c2fad24d8fc
MarianaD <marianadelgadopaez@gmail.com>
2023-09-18 16:20:50 -0300

    este commit revierte el anterior donde probabamos

--- index.php
+++ index.php
@@ -12,8 +12,5 @@
     <p>primer cambio en repositorio en local <p>
     <p>Prueba cambiar en repositorio local</p>
     <p>Cambiando en el repositorio remoto </p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, quisquam nemo. Libero architecto fugit reprehenderit, commodi consequatur iusto ad ipsum nemo aliquid repellendus pariatur similique fuga hic, amet eveniet natus.</p>
-    <p>Ahora agrego un lorem para probar el stash</p>
-    <p>Agregando nuevo commit para revertir luego<p>
 </body>
 </html>
